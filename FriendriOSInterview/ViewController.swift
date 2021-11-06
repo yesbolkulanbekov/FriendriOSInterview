b8e5bc254f97913838c9150b07d026e60dae5097
Yesbol Kulanbekov <esbolkz@yahoo.com>
2021-11-07 03:01:41 +0600

    Initial Commit

---
+++ FriendriOSInterview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FriendriOSInterview
+//
+//  Created by Yesbol Kulanbekov on 11/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
